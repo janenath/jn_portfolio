4d32ee7652de7e97aa0d0e881dc685543786e9b0
Jane <jane.e.nath@gmail.com>
2020-05-22 15:46:15 -0700

    add projects to resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,6 +5,42 @@ class Resume extends Component {
         return(
             <div className="page">
                 <h3>to download a printable copy of my resume, <a className="textLink" href="https://janenath.s3.amazonaws.com/Jane+Nath+Resume.pdf" target="blank" download>click here.</a></h3>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <h4>selected projects:</h4>
+                <div className="projects">
+                    <div className="projectsRow">
+                        <div className="project">
+                            <a href="https://github.com/janenath/fourth_trimester" target="_blank">
+                            <p>Fourth Trimester | Full-Stack App with React, Ruby on Rails</p>
+                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/fourth_trimester.png" alt="fourth trimester logo"></img>
+                            </a>
+                        </div>
+                        <div className="project">
+                            <a href="https://github.com/janenath/capsule" target="_blank">
+                            <p>Capsule | RESTful API with React, Ruby on Rails</p>
+                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/capsule.png" alt="capsule logo"></img>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="projectsRow">
+                        <div className="project">
+                            <a href="https://github.com/janenath/plot-master" target="_blank">
+                            <p>Plot Master | Full-Stack App with React, Node.js</p>
+                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/plot_master.png" alt="plot master logo"></img>
+                            </a>
+                        </div>
+                        <div className="project">
+                            <a href="https://github.com/janenath/elias_miller" target="_blank">
+                            <p>EM Portfolio | Jamstack App with Gatsby, Contentful</p>
+                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/em.png" alt="elias miller portfolio"></img>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }

fdc526a98cab7eb916fd72c59b174a28cfaf1095
Jane <jane.e.nath@gmail.com>
2020-05-22 17:08:02 -0700

    remove projects from mobile view

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,37 +10,39 @@ class Resume extends Component {
                 <br/>
                 <br/>
                 <br/>
-                <h4>selected projects:</h4>
-                <div className="projects">
-                    <div className="projectsRow">
-                        <div className="project">
-                            <a href="https://github.com/janenath/fourth_trimester" target="_blank">
-                            <p>Fourth Trimester | Full-Stack App with React, Ruby on Rails</p>
-                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/fourth_trimester.png" alt="fourth trimester logo"></img>
-                            </a>
+                <div className="projectContainer">
+                    <h4>selected projects:</h4>
+                    <div className="projects">
+                        <div className="projectsRow">
+                            <div className="project">
+                                <a href="https://github.com/janenath/fourth_trimester" target="_blank">
+                                <p>Fourth Trimester | Full-Stack App with React, Ruby on Rails</p>
+                                <img className="appPreview" src="https://janenath.s3.amazonaws.com/fourth_trimester.png" alt="fourth trimester logo"></img>
+                                </a>
+                            </div>
+                            <div className="project">
+                                <a href="https://github.com/janenath/capsule" target="_blank">
+                                <p>Capsule | RESTful API with React, Ruby on Rails</p>
+                                <img className="appPreview" src="https://janenath.s3.amazonaws.com/capsule.png" alt="capsule logo"></img>
+                                </a>
+                            </div>
                         </div>
-                        <div className="project">
-                            <a href="https://github.com/janenath/capsule" target="_blank">
-                            <p>Capsule | RESTful API with React, Ruby on Rails</p>
-                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/capsule.png" alt="capsule logo"></img>
-                            </a>
+                        <div className="projectsRow">
+                            <div className="project">
+                                <a href="https://github.com/janenath/plot-master" target="_blank">
+                                <p>Plot Master | Full-Stack App with React, Node.js</p>
+                                <img className="appPreview" src="https://janenath.s3.amazonaws.com/plot_master.png" alt="plot master logo"></img>
+                                </a>
+                            </div>
+                            <div className="project">
+                                <a href="https://github.com/janenath/elias_miller" target="_blank">
+                                <p>EM Portfolio | Jamstack App with Gatsby, Contentful</p>
+                                <img className="appPreview" src="https://janenath.s3.amazonaws.com/em.png" alt="elias miller portfolio"></img>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div className="projectsRow">
-                        <div className="project">
-                            <a href="https://github.com/janenath/plot-master" target="_blank">
-                            <p>Plot Master | Full-Stack App with React, Node.js</p>
-                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/plot_master.png" alt="plot master logo"></img>
-                            </a>
-                        </div>
-                        <div className="project">
-                            <a href="https://github.com/janenath/elias_miller" target="_blank">
-                            <p>EM Portfolio | Jamstack App with Gatsby, Contentful</p>
-                            <img className="appPreview" src="https://janenath.s3.amazonaws.com/em.png" alt="elias miller portfolio"></img>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                </div>          
             </div>
         )
     }
